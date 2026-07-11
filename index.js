d1f89298658682760236be5a2f3f383fcd60f2a1
ratebbarish12-boop <rateb.barish12@gmail.com>
2026-07-11 19:40:02 +0430

    Add express server for Render

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 const pino = require("pino");
 const qrcode = require("qrcode-terminal");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
 
 async function startBot() {
   const { state, saveCreds } = await useMultiFileAuthState("auth");
