9325947a42bc5bb18c9fc7eebb6cc8c098b0cdc9
ratebbarish12-boop <rateb.barish12@gmail.com>
2026-07-11 16:54:28 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-console.log("Bot is running...");
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("Bot is running");
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 setInterval(() => {
   console.log("Bot is still alive");
